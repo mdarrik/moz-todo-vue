5a7bd0caf18c597f751e25f32db985be6015df89
Darrik Moberg <30670444+mdarrik@users.noreply.github.com>
2020-03-15 20:47:34 -0700

    Sets up the To-do app up to styling

---
+++ src/App.vue
@@ -0,0 +1,156 @@
+<template>
+  <div id="app">
+    <h1>My To-Do List</h1>
+    <to-do-form @todo-added="addToDo($event)" />
+    <ul>
+      <li v-for="item in ToDoItems" :key="item.id">
+        <to-do-item :label="item.name" :done="item.done" :id="item.id" />
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import uniqueId from 'lodash.uniqueid'
+import ToDoItem from './components/ToDoItem.vue';
+import ToDoForm from './components/ToDoForm';
+
+export default {
+  name: 'app',
+  components: {
+    ToDoItem,
+    ToDoForm
+  },
+ data() {
+    return {
+      ToDoItems: [
+        { id:uniqueId('todo-'), name: 'Learn Vue', done: false },
+        { id:uniqueId('todo-'), name: 'Create a Vue project with the CLI', done: true },
+        { id:uniqueId('todo-'), name: 'Have fun', done: true },
+        { id:uniqueId('todo-'), name: 'Create a to-do list', done: false }
+      ]
+    };
+  },
+  methods: {
+    addToDo(toDoName) {
+      this.ToDoItems.push({id:uniqueId('todo-'), name: toDoName, done: false})
+    }
+  }
+};
+</script>
+
+<style>
+/* Global styles */
+
+.btn {
+  padding: 0.8rem 1rem 0.7rem;
+  border: 0.2rem solid #4d4d4d;
+  cursor: pointer;
+  text-transform: capitalize;
+}
+
+.btn__danger {
+  color: #fff;
+  background-color: #ca3c3c;
+  border-color: #bd2130;
+}
+
+.btn__filter {
+  border-color: lightgrey;
+}
+
+.btn__danger:focus {
+  outline-color: #c82333;
+}
+
+.btn__primary {
+  color: #fff;
+  background-color: #000;
+}
+
+.btn-group {
+  display: flex;
+  justify-content: space-between;
+}
+
+.btn-group > * {
+  flex: 1 1 auto;
+}
+
+.btn-group > * + * {
+  margin-left: 0.8rem;
+}
+
+.label-wrapper {
+  margin: 0;
+  flex: 0 0 100%;
+  text-align: center;
+}
+
+.label__lg {
+  line-height: 1.01567;
+  font-weight: 300;
+  padding: 0.8rem;
+  margin-bottom: 1rem;
+  text-align: center;
+}
+
+.input__lg {
+  padding: 2rem;
+  border: 2px solid #000;
+}
+
+.input__lg:focus {
+  border-color: #4d4d4d;
+  box-shadow: inset 0 0 0 2px;
+}
+
+[class*="__lg"] {
+  display: inline-block;
+  width: 100%;
+  font-size: 1.9rem;
+}
+
+[class*="__lg"]:not(:last-child) {
+  margin-bottom: 1rem;
+}
+
+@media screen and (min-width: 620px) {
+  [class*="__lg"] {
+    font-size: 2.4rem;
+  }
+}
+/* End global styles */
+#app {
+  background: #fff;
+  margin: 2rem 0 4rem 0;
+  padding: 1rem;
+  padding-top: 0;
+  position: relative;
+  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2), 0 2.5rem 5rem 0 rgba(0, 0, 0, 0.1);
+}
+
+@media screen and (min-width: 550px) {
+  #app {
+    padding: 4rem;
+  }
+}
+
+#app > * {
+  max-width: 50rem;
+  margin-left: auto;
+  margin-right: auto;
+}
+#app > form {
+  max-width: 100%;
+}
+
+#app h1 {
+  display: block;
+  min-width:100%;
+  width: 100%;
+  text-align: center;
+  margin: 0;
+  margin-bottom: 1rem;
+}
+</style>
