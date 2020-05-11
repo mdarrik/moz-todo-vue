03e609f74104266b0c49061abcb67aa5222f5ead
Darrik Moberg <30670444+mdarrik@users.noreply.github.com>
2020-05-11 01:00:03 -0700

    style: adds stack-large to list

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <h1>My To-Do List</h1>
     <to-do-form @todo-added="addToDo" />
     <h2 id="list-summary">{{listSummary}}</h2>
-    <ul aria-labelledby="list-summary">
+    <ul aria-labelledby="list-summary" class="stack-large">
       <li v-for="item in ToDoItems" :key="item.id">
         <to-do-item :label="item.name" :done="item.done" :id="item.id" @checkbox-changed="updateDoneStatus(item.id)"></to-do-item>
       </li>
